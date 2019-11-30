f9b9258220a16b54aaf081976d00340a9e7eb556
Tomas Vaedelund <tvaedelund@gmail.com>
2019-11-30 09:31:48 +0100

    Y2015.Day01

--- Days/Y2015/Day01.cs
+++ Days/Y2015/Day01.cs
@@ -1,12 +1,88 @@
 using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace advent_of_code.Days.Y2015
 {
     public static class Day01
     {
-        public static void Run()
+        public async static void Run()
         {
-            Console.WriteLine("Y2015.Day01");
+            // Initialize
+            var day = "Day01";
+            var year = "2015";
+            Console.WriteLine($"Y{year}.{day}");
+
+            // Load resources
+            var data = await GetDataAsync(year, day);
+
+            // Step 1
+            Debug.Assert(GetResultOne("(())") == 0);
+            Debug.Assert(GetResultOne(("()()")) == 0);
+            Debug.Assert(GetResultOne("(((") == 3);
+            Debug.Assert(GetResultOne("(()(()(") == 3);
+            Debug.Assert(GetResultOne("))(((((") == 3);
+            Debug.Assert(GetResultOne("())") == -1);
+            Debug.Assert(GetResultOne("))(") == -1);
+            Debug.Assert(GetResultOne(")))") == -3);
+            Debug.Assert(GetResultOne(")())())") == -3);
+
+            var timerOne = Stopwatch.StartNew();
+            var resultOne = GetResultOne(data);
+            Console.WriteLine($"Result 1: {resultOne}");
+            Console.WriteLine($"Done in {timerOne.ElapsedMilliseconds} ms");
+
+            // Step 2
+            Debug.Assert(GetResultTwo(")") == 1);
+            Debug.Assert(GetResultTwo("()())") == 5);
+
+            var timerTwo = Stopwatch.StartNew();
+            var resultTwo = GetResultTwo(data);
+            Console.WriteLine($"Result 2: {resultTwo}");
+            Console.WriteLine($"Done in {timerTwo.ElapsedMilliseconds} ms");
+        }
+
+        private static async Task<string> GetDataAsync(string year, string day)
+        {
+            return await File.ReadAllTextAsync(@$".\Inputs\Y{year}\{day.ToLower()}.txt");
+        }
+
+        private static int GetResultOne(string data)
+        {
+            var start = 0;
+            var instructions = data.ToCharArray();
+
+            foreach (var instr in instructions)
+            {
+                start += (instr == '(') ? 1 : 0;
+                start -= (instr == ')') ? 1 : 0;
+            }
+
+            return start;
+        }
+
+        private static int GetResultTwo(string data)
+        {
+            var start = 0;
+            var pos = 0;
+            var instructions = data.ToCharArray();
+
+            for (int i = 0; i < instructions.Length; i++)
+            {
+                var instr = instructions[i];
+
+                start += (instr == '(') ? 1 : 0;
+                start -= (instr == ')') ? 1 : 0;
+
+                if (start == -1)
+                {
+                    pos = i + 1;
+                    break;
+                }
+            }
+
+            return pos;
         }
     }
 }
